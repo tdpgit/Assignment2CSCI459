04db0b4451c45e3db8ab795d89cb08cbe7a37597
Trevor Priestley <Trevor@dyn216-43.wireless-1208.nat.ndsu.nodak.edu>
2015-10-26 12:22:06 -0500

    Initial Commit

---
+++ CSCI459Assignment4/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSCI459Assignment4
+//
+//  Created by Trevor Priestley on 10/26/15.
+//  Copyright © 2015 Trevor Priestley. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
